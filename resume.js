b914c59237544704b52eb47024a002bc0d020b0d
Hayden <chfrank9011@gmail.com>
2019-03-24 12:03:25 -0500

    final commit with formatting for readability

--- resume.js
+++ resume.js
@@ -8,9 +8,9 @@ function displayPosition(jobDetail) {
 
 function displaySkills(skills, cool) {
     if (cool) {
-        console.log("* BAM: " + skills);
+        console.log('* BAM: ' + skills);
     } else {
-        console.log("* " + skills);
+        console.log('* ' + skills);
     }
 }
 
@@ -20,18 +20,32 @@ function displayInterests() {
         console.log("* " + i);
     }
 }
+
 console.log('* Name: ' + myName.toUpperCase())
+
 console.log('* Career: Full Stack Developer... Currently unemployed.');
+
 console.log('* Mini-bio: Nerd who likes books and D&D. Not a whole lot else.');
+
 displayInterests();
+
 console.log('\nMost Recent Position Held:');
+
 displayPosition('General Manager');
+
 displayPosition('Revelator: Sweet Jons');
+
 displayPosition('Manage a small team of people to effectivly run a cafe, provide excellent customer service, and produce high quality food and coffee.')
+
 console.log('\nMy mad skillzzzz:');
+
 displaySkills('Barista', false);
+
 displaySkills('Dungeon Mastering', true);
+
 displaySkills('Bo staff', true);
+
 displaySkills('Cat petting', true);
+
 displaySkills('Power sleeping', true);
 
